f98136a7cabd20f061c0057170c1d5da18d4727e
fridaharju <fridaharju@lifesum.com>
2016-09-08 15:53:18 +0200

    style
    styling the page

--- quiz/views.py
+++ quiz/views.py
@@ -1,19 +1,25 @@
+
 quizzes = [
 	{
 		"quiz_number": 1,
    		"name": "Klassiska böcker",
-	   	"description": "Hur bra kan du dina klassiker?"
+	   	"description": "Hur bra kan du dina klassiker?",
+	   	"image" : "https://cdn1.cdnme.se/1024491/9-3/dsc05048_572a54a3ddf2b35586374f95.jpg"
 	},
 	{
 		"quiz_number": 2,
    	   	"name": "Största 1slagen",
-	   	"description": "Kan du dina lag?"
+	   	"description": "Kan du dina lag?",
+	   	"image": "https://cdn1.cdnme.se/1024491/7-3/aaaaaaaa_54117af62a6b22195b43bb70.jpg"
 	},
 	{
 		"quiz_number": 3,
    	    	"name": "Världens mest kända hackare",
-	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+	    	"description": "Hackerhistoria är viktigt, kan du den?",
+	    	"image": "https://d3vjk4jagnknqc.cloudfront.net/uploads/2013/11/f1190198-2048x1151.jpg",
+	    	}
 ]
+
 
 
 from django.shortcuts import render
