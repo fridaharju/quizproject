88dd63bdeb451d5480a0d6f8d5019ec27e8c8960
fridaharju <fridaharju@lifesum.com>
2016-09-08 11:33:15 +0200

    views
    changing names to my own

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+def startpage(request):
+return render(request, "quiz/start.html")
+def quiz(request):
+return render(request, "quiz/quiz1.html")
+def question(request):
+return render(request, "quiz/question.html")
+def completed(request):
+return render(request, "quiz/result.html")
+
 
 # Create your views here.
