dfa410a6c72c1ac30778a7ede21fbf8e50453fe2
fridaharju <fridaharju@lifesum.com>
2016-09-09 10:32:05 +0200

    change name
    on questions and answers

--- quiz/models.py
+++ quiz/models.py
@@ -3,7 +3,9 @@ class Quiz(models.Model):
 	quiz_number = models.PositiveIntegerField()
 	name = models.CharField(max_length=100)
 	description = models.TextField()
-	
+	def __str__(self):
+		return self.name
+
 class Question(models.Model):
 	question = models.TextField()
 	answer1 = models.CharField(max_length=100)
@@ -11,3 +13,7 @@ class Question(models.Model):
 	answer3 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
 	quiz = models.ForeignKey(Quiz, related_name="questions")
+	def __str__(self):
+		return self.quiz.name + " / " + self.question
+
+	
