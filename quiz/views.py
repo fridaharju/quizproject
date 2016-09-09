4cf938a2b3d9381d96939572a08df8e941b47482
fridaharju <fridaharju@lifesum.com>
2016-09-09 13:52:40 +0200

    gör så att det funkar
    pip3 istället för pip

--- quiz/views.py
+++ quiz/views.py
@@ -2,31 +2,31 @@ from django.shortcuts import redirect
 from quiz.models import Quiz
 from django.shortcuts import render
 def start(request):
-	context = {
-		"quizzes": Quiz.objects.all(),
-	}
-	return render(request, "quiz/start.html", context)
+    context = {
+        "quizzes": Quiz.objects.all(),
+    }
+    return render(request, "quiz/start.html", context)
 def quiz(request, quiz_number):
-	context = {
-		"quiz": Quiz.objects.get(quiz_number=quiz_number),
-		"quiz_number": quiz_number,
-	}
-	return render(request, "quiz/quiz1.html", context)
+    context = {
+        "quiz": Quiz.objects.get(quiz_number=quiz_number),
+        "quiz_number": quiz_number,
+    }
+    return render(request, "quiz/quiz1.html", context)
 def question(request, quiz_number, question_number):
-	quiz = Quiz.objects.get(quiz_number=quiz_number)
-	questions = quiz.questions.all()
-	question = questions[int(question_number) - 1]
-	context = {
-    	"question_number": question_number,
-    	"question": question.question,
-	    "answer1": question.answer1,
-    	"answer2": question.answer2,
-	    "answer3": question.answer3,
-	    "quiz": quiz,
-		"quiz_number": quiz_number,
-	}
+    quiz = Quiz.objects.get(quiz_number=quiz_number)
+    questions = quiz.questions.all()
+    question = questions[int(question_number) - 1]
+    context = {
+        "question_number": question_number,
+        "question": question.question,
+        "answer1": question.answer1,
+        "answer2": question.answer2,
+        "answer3": question.answer3,
+        "quiz": quiz,
+        "quiz_number": quiz_number,
+    }
 
-	return render(request, "quiz/question.html", context)
+    return render(request, "quiz/question.html", context)
 def answer(request, quiz_number, question_number):
     saved_answers = request.session.get(quiz_number, {})
     answer = int(request.POST["answer"])
@@ -34,30 +34,30 @@ def answer(request, quiz_number, question_number):
     request.session[quiz_number] = saved_answers
 
     
-	question_number = int(question_number):
-	quiz = Quiz.objects.get(quiz_number=quiz_number)
-	num_questions = quiz.questions.count()
-	if num_questions <= question_number:
-    	return redirect("result_page", quiz_number)
-	else:
-    	return redirect("question_page", quiz_number, question_number + 1)
+    question_number = int(question_number)
+    quiz = Quiz.objects.get(quiz_number=quiz_number)
+    num_questions = quiz.questions.count()
+    if num_questions <= question_number:
+        return redirect("result_page", quiz_number)
+    else:
+        return redirect("question_page", quiz_number, question_number + 1)
 
 def result(request, quiz_number):
-	quiz = Quiz.objects.get(quiz_number=quiz_number)
-	questions = quiz.questions.all()
-	saved_answers = request.session.get(quiz_number, {})
+    quiz = Quiz.objects.get(quiz_number=quiz_number)
+    questions = quiz.questions.all()
+    saved_answers = request.session.get(quiz_number, {})
 
-	num_correct_answers = 0
-	for question_number, answer in saved_answers.items():
-    	correct_answer = questions[int(question_number) - 1].correct
-    	if correct_answer == answer:
-	        num_correct_answers += 1
+    num_correct_answers = 0
+    for question_number, answer in saved_answers.items():
+        correct_answer = questions[int(question_number) - 1].correct
+        if correct_answer == answer:
+            num_correct_answers += 1
 
-	context = {
-    	"correct": num_correct_answers,
-    	"total": questions.count(),
-	}
-	return render(request, "quiz/result.html", context)
+    context = {
+        "correct": num_correct_answers,
+        "total": questions.count(),
+    }
+    return render(request, "quiz/result.html", context)
 
 
 
